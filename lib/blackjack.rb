88aadff6001ff86ea16f206d2440a86097e8108d
Alexander Sahli <alexander.m.sahli@gmail.com>
2020-08-02 23:54:00 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -60,11 +60,7 @@ def runner
   card_total = initial_round
   while card_total < 21
   card_total = hit?(card_total)
-  if invalid_command do
-    prompt_user
-  else
   card_total = display_card_total(card_total)
-end
 end
 end_game(card_total)
 end
